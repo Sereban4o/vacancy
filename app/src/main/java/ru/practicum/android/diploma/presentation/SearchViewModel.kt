da52e0d83ca6dcfb08e228ae0906884fc89263c0
Ecballium elaterium <amanzholaimov@gmail.com>
2025-11-24 00:22:24 +0500

    fix: detekt import ordering for SearchViewModel

--- app/src/main/java/ru/practicum/android/diploma/presentation/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/SearchViewModel.kt
@@ -1,13 +1,17 @@
 package ru.practicum.android.diploma.presentation
 
 import android.util.Log
+
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+
 import java.io.IOException
+
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+
 import ru.practicum.android.diploma.domain.interactors.SearchVacanciesInteractor
 import ru.practicum.android.diploma.ui.main.SearchErrorType
 import ru.practicum.android.diploma.ui.main.SearchUiState
