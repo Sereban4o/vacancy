b2d19557dfc3641bcf85a92810755d793edb116c
Ecballium elaterium <amanzholaimov@gmail.com>
2025-12-02 21:16:59 +0500

    fix detekt1

--- app/src/main/java/ru/practicum/android/diploma/ui/workplace/WorkPlaceRow.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/workplace/WorkPlaceRow.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.ui.workplace
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
